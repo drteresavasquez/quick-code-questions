629c08ef99525753a381aa4f3c83120d43a3ed7f
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-01-20 13:30:26 -0600

    new challenge

---
+++ empty-that-array.js
@@ -0,0 +1,11 @@
+// I have this array:
+let array = ['Start Doing', 'Continue Doing', 'Stop Doing'];
+// I like it just fine, but there is a time in my code that I need to empty that array out and start over again. Can anyone tell me how I could empty an array?
+
+array = [];
+console.log(array);
+
+// Last one...So, ___ helped me figure out how to empty my array. Now, someone else tell me if I do this, what value will be console logged?
+const outfit = ['jacket', 't-shirt', 'pants', 'knee-high socks'];
+outfit.length = 0;
+console.log(outfit[0]); 
---
+++ oops-loop.js
@@ -0,0 +1,8 @@
+// I was writing a loop and suddenly everything froze! My browser would not stop refreshing...I had to shut down my computer!
+
+// Look at this loop and tell me 1. What is this called and 2. Why did it happen and how can I prevent it from happening again...
+
+// DO NOT RUN THIS EVER!
+// for (let i = 0; i >= 0; i++) {
+//   console.log(i);
+// }
